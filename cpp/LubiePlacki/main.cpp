ca33a7fb1e18eb61eaf1dad88f5947618c79c67f
Soapz7 <alan.soap606@gmail.com>
2025-10-22 11:10:12 +0000

    studentmachine send

--- cpp/LubiePlacki/main.cpp
+++ cpp/LubiePlacki/main.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main(){
-	cout<<"witaj swiecie"<<endl;
+	cout<<"podaj x" <<endl;
+	int x;
+	cin >> x;
+	cout<<"podano " << x << endl;
+	if (x>0) {
+		cout<<"wieksze od 0 \n";
+	} else {
+		cout<<"mniejsze lub rowne 0 \n";
+	}
+
 	return 0;
 }
