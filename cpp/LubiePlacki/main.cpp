5898c1662bddad766921c6c0b338701ff2678ec1
Soapz7 <alan.soap606@gmail.com>
2025-11-12 12:12:09 +0000

    studentmachine send

--- cpp/LubiePlacki/main.cpp
+++ cpp/LubiePlacki/main.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 
 int main(){
-	cout<<"podaj x" <<endl;
+	cout <<"podaj x" <<endl;
 	int x;
 	cin >> x;
-	cout<<"podano " << x << endl;
-	if (x>0) {
-		cout<<"wieksze od 0 \n";
+	if(x % 2 == 0 && x & (x / x) == 0){
+		cout << "x jest liczba pierwsza" << endl;
 	} else {
-		cout<<"mniejsze lub rowne 0 \n";
+		cout << "x nie jest liczba pierwsza" << endl;
 	}
 
 	return 0;
